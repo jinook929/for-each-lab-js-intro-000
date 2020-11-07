24abbcc133a40f1ae42ef38947319f58e0cef1ac
JINOOK JUNG <jinook929@gmail.com>
2020-11-07 12:18:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ function iterativeLog(arr) {
   arr.forEach((item, index) => console.log(`${index}: ${element}`));
 }
 
-function iterate(item, index, array) {
+function iterate(myFunc) {
   let arr = [];
   arr[index] = `${index}: ${element}`
   return arr;
 }
 
-function iterativeLog(arr, myFunc) {
+function doToArray(item, index, array) {
   arr.forEach(myFunc);
 }
